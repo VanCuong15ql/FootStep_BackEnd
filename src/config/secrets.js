2ba4bf88074ece1bb07edfd089bb27de69abe4a2
vancuong15ql <vancuongbui15ql@gmail.com>
2024-11-26 13:23:48 +0700

    no change

--- src/config/secrets.js
+++ src/config/secrets.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 4000;
 const DB_URL = process.env.DB_URL ?? "mongodb+srv://api-user:<PASSWORD>@cluster0.p4pja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const DB_PASSWORD = process.env.DB_PASSWORD ?? "XX4Oas8BOuikWYij";
 const JWT_SECRET = process.env.JWT_SECRET ?? "";
